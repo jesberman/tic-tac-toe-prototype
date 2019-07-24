5ba80222d8e4184767b4b4c466450cc205a381f7
jesberman <jesberman@gmail.com>
2019-07-24 14:38:59 -0400

    initial-commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,33 @@
+
+// function markX(td) {
+//     var name = td.id;
+//     alert(name);
+// }
+
+function markX1() {
+    document.getElementById("cell1").innerHTML = "X";
+}
+function markX2() {
+    document.getElementById("cell2").innerHTML = "X";
+}
+function markX3() {
+    document.getElementById("cell3").innerHTML = "X";
+}
+function markX4() {
+    document.getElementById("cell4").innerHTML = "X";
+}
+function markX5() {
+    document.getElementById("cell5").innerHTML = "X";
+}
+function markX6() {
+    document.getElementById("cell6").innerHTML = "X";
+}
+function markX7() {
+    document.getElementById("cell7").innerHTML = "X";
+}
+function markX8() {
+    document.getElementById("cell8").innerHTML = "X";
+}
+function markX9() {
+    document.getElementById("cell9").innerHTML = "X";
+}
