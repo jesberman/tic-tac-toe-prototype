bc8e3a94ae3a7cabac68da927dc7053c99ac8df2
jesberman <jesberman@gmail.com>
2019-07-25 16:33:00 -0400

    added-functionality-code-now-recognizes-when-player-loses

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,140 +1,260 @@
 
-// function markX(td) {
-//     var name = td.id;
-//     alert(name);
-// }
 var availableCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-console.log(availableCells);
-let xCell0 = false;
-let xCell1 = false;
-let xCell2 = false;
-let xCell3 = false;
-let xCell4 = false;
-let xCell5 = false;
-let xCell6 = false;
-let xCell7 = false;
-let xCell8 = false;
+
+let xCell0 = 0;
+let xCell1 = 0;
+let xCell2 = 0;
+let xCell3 = 0;
+let xCell4 = 0;
+let xCell5 = 0;
+let xCell6 = 0;
+let xCell7 = 0;
+let xCell8 = 0;
+
+var maxClicks = 5;
 
 function markO() {
     var oMove = Math.floor((Math.random() * 9));
-    console.log(oMove);
-    if (availableCells[oMove] === "filled") {
+
+    if (maxClicks === 0) {
+        return;
+    }
+    else if (availableCells[oMove] === "filled") {
         oMove = Math.floor((Math.random() * 9));
+        markO();
     }
     else {
         document.getElementById("cell" + oMove).innerHTML = "O";
         document.getElementById("cell" + oMove).onclick = "";
+        availableCells.splice(oMove, 1, "filled");
+        var cell0Value = document.getElementById("cell0").innerHTML;
+        if (cell0Value === "O") {
+            xCell0 = "o";
+        }
+        var cell1Value = document.getElementById("cell1").innerHTML;
+        if (cell1Value === "O") {
+            xCell1 = "o";
+        }
+        var cell2Value = document.getElementById("cell2").innerHTML;
+        if (cell2Value === "O") {
+            xCell2 = "o";
+        }
+        var cell3Value = document.getElementById("cell3").innerHTML;
+        if (cell3Value === "O") {
+            xCell3 = "o";
+        }
+        var cell4Value = document.getElementById("cell4").innerHTML;
+        if (cell4Value === "O") {
+            xCell4 = "o";
+        }
+        var cell5Value = document.getElementById("cell5").innerHTML;
+        if (cell5Value === "O") {
+            xCell5 = "o";
+        }
+        var cell6Value = document.getElementById("cell6").innerHTML;
+        if (cell6Value === "O") {
+            xCell6 = "o";
+        }
+        var cell7Value = document.getElementById("cell7").innerHTML;
+        if (cell7Value === "O") {
+            xCell7 = "o";
+        }
+        var cell8Value = document.getElementById("cell8").innerHTML;
+        if (cell8Value === "O") {
+            xCell8 = "o";
+        }
     }
 }
 
 function playerWinCheck() {
-    if (xCell0 === true && xCell1 === true && xCell2 === true) {
-        alert("You Win!");
+    if (xCell0 === "x" && xCell1 === "x" && xCell2 === "x") {
+        $("#cell0").css("color", "red");
+        $("#cell1").css("color", "red");
+        $("#cell2").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell3 === true && xCell4 === true && xCell5 === true) {
-        alert("You Win!");
+    if (xCell3 === "x" && xCell4 === "x" && xCell5 === "x") {
+        $("#cell3").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell5").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell6 === true && xCell7 === true && xCell8 === true) {
-        alert("You Win!");
+    if (xCell6 === "x" && xCell7 === "x" && xCell8 === "x") {
+        $("#cell6").css("color", "red");
+        $("#cell7").css("color", "red");
+        $("#cell8").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell0 === true && xCell3 === true && xCell6 === true) {
-        alert("You Win!");
+    if (xCell0 === "x" && xCell3 === "x" && xCell6 === "x") {
+        $("#cell0").css("color", "red");
+        $("#cell3").css("color", "red");
+        $("#cell6").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell1 === true && xCell4 === true && xCell7 === true) {
-        alert("You Win!");
+    if (xCell1 === "x" && xCell4 === "x" && xCell7 === "x") {
+        $("#cell1").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell7").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell2 === true && xCell5 === true && xCell8 === true) {
-        alert("You Win!");
+    if (xCell2 === "x" && xCell5 === "x" && xCell8 === "x") {
+        $("#cell2").css("color", "red");
+        $("#cell5").css("color", "red");
+        $("#cell8").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell0 === true && xCell4 === true && xCell8 === true) {
-        alert("You Win!");
+    if (xCell0 === "x" && xCell4 === "x" && xCell8 === "x") {
+        $("#cell0").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell8").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
     }
-    if (xCell2 === true && xCell4 === true && xCell6 === true) {
-        alert("You Win!");
+    if (xCell2 === "x" && xCell4 === "x" && xCell6 === "x") {
+        $("#cell2").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell6").css("color", "red");
+        setTimeout(function(){ alert("You Win!"); }, 50);
+    }
+}
+
+function playerLoseCheck() {
+    if (xCell0 === "o" && xCell1 === "o" && xCell2 === "o") {
+        $("#cell0").css("color", "red");
+        $("#cell1").css("color", "red");
+        $("#cell2").css("color", "red");
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+    }
+    if (xCell3 === "o" && xCell4 === "o" && xCell5 === "o") {
+        $("#cell3").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell5").css("color", "red");
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+    }
+    if (xCell6 === "o" && xCell7 === "o" && xCell8 === "o") {
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+        $("#cell6").css("color", "red");
+        $("#cell7").css("color", "red");
+        $("#cell8").css("color", "red");
+    }
+    if (xCell0 === "o" && xCell3 === "o" && xCell6 === "o") {
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+        $("#cell0").css("color", "red");
+        $("#cell3").css("color", "red");
+        $("#cell6").css("color", "red");
+    }
+    if (xCell1 === "o" && xCell4 === "o" && xCell7 === "o") {
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+        $("#cell1").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell7").css("color", "red");
+    }
+    if (xCell2 === "o" && xCell5 === "o" && xCell8 === "o") {
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+        $("#cell2").css("color", "red");
+        $("#cell5").css("color", "red");
+        $("#cell8").css("color", "red");
+    }
+    if (xCell0 === "o" && xCell4 === "o" && xCell8 === "o") {
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+        $("#cell0").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell8").css("color", "red");
+    }
+    if (xCell2 === "o" && xCell4 === "o" && xCell6 === "o") {
+        setTimeout(function(){ alert("Sorry, you lose.  Please try again."); }, 50);
+        $("#cell2").css("color", "red");
+        $("#cell4").css("color", "red");
+        $("#cell6").css("color", "red");
     }
 }
 function markX0() {
+    maxClicks -= 1;
     document.getElementById("cell0").innerHTML = "X";
-    xCell0 = true;
+    document.getElementById("cell0").onclick = "";
+    xCell0 = "x";
     availableCells.splice(0, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX1() {
+    maxClicks -= 1;
     document.getElementById("cell1").innerHTML = "X";
-    xCell1 = true;
+    document.getElementById("cell1").onclick = "";
+    xCell1 = "x";
     availableCells.splice(1, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX2() {
+    maxClicks -= 1;
     document.getElementById("cell2").innerHTML = "X";
-    xCell2 = true;
+    document.getElementById("cell2").onclick = "";
+    xCell2 = "x";
     availableCells.splice(2, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX3() {
+    maxClicks -= 1;
     document.getElementById("cell3").innerHTML = "X";
-    xCell3 = true;
+    document.getElementById("cell3").onclick = "";
+    xCell3 = "x";
     availableCells.splice(3, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX4() {
+    maxClicks -= 1;
     document.getElementById("cell4").innerHTML = "X";
-    xCell4 = true;
+    document.getElementById("cell4").onclick = "";
+    xCell4 = "x";
     availableCells.splice(4, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX5() {
+    maxClicks -= 1;
     document.getElementById("cell5").innerHTML = "X";
-    xCell5 = true;
+    document.getElementById("cell5").onclick = "";
+    xCell5 = "x";
     availableCells.splice(5, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX6() {
+    maxClicks -= 1;
     document.getElementById("cell6").innerHTML = "X";
-    xCell6 = true;
+    document.getElementById("cell6").onclick = "";
+    xCell6 = "x";
     availableCells.splice(6, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX7() {
+    maxClicks -= 1;
     document.getElementById("cell7").innerHTML = "X";
-    xCell7 = true;
+    document.getElementById("cell7").onclick = "";
+    xCell7 = "x";
     availableCells.splice(7, 1, "filled");
-    console.log(availableCells);
     markO();
-
     playerWinCheck();
+    playerLoseCheck();
 }
 function markX8() {
+    maxClicks -= 1;
     document.getElementById("cell8").innerHTML = "X";
-    xCell8 = true;
+    document.getElementById("cell8").onclick = "";
+    xCell8 = "x";
     availableCells.splice(8, 1, "filled");
-    console.log(availableCells);
     markO();
     playerWinCheck();
+    playerLoseCheck();
 }
 
-
-
-
-// markO();
